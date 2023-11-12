6c93f4663cdd5b7e4e308fca9d907729086251ff
Slayery777 <vladislavbrus1@gmail.com>
2023-11-13 00:20:59 +0200

    LR3 fix

--- lr3SRC/src/main/java/com/example/MyAspect.java
+++ lr3SRC/src/main/java/com/example/MyAspect.java
@@ -4,8 +4,10 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class MyAspect {
 
     @Pointcut("execution(* com.example.BeanA.doSomething(..))")
